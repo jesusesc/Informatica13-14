f8f3977f560a4caff4762edca0303f481eddb0d7
Jesus <jesusesc@ucm.es>
2013-12-05 20:13:59 +0100

    ejemplo

---
+++ sample.py
@@ -0,0 +1,27 @@
+def cambio(n):
+    # instrucción1
+    # instrucción2
+    # .....
+    # instrucción_k
+    # SOLUCIÓN: n2e monedas de 2 euros, n1e monedas de 1 euro, n50c monedas de 50 cent.,
+    # n20c monedas de 20 cent, n10c monedas de 10 cent, n5c monedas de 5 cent,
+    # n2c monedas de 2 cent. y n1c monedas de 1 cent.
+    return n2e, n1e, n50c, n20c, n10c, n5c, n2c, n1c
+
+def cambio_con_limite(n):
+    #constantes
+    N2E = 100 #número de monedas de 2 euros
+    N1E = 50 # número de monedas de 1 euro
+    N50C = 20 # ....
+    # .....
+
+    # instrucción1
+    # instrucción2
+    # .....
+    # instrucción_k
+    # SOLUCIÓN: n2e monedas de 2 euros, n1e monedas de 1 euro, n50c monedas de 50 cent.,
+    # n20c monedas de 20 cent, n10c monedas de 10 cent, n5c monedas de 5 cent,
+    # n2c monedas de 2 cent. y n1c monedas de 1 cent.
+    return n2e, n1e, n50c, n20c, n10c, n5c, n2c, n1c
+
+
